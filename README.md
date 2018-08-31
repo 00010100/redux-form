73233ed660a8eed9bcba88579a37972a70c6f896
00010100 <0001101004@protonmail.com>
2018-08-31 16:54:14 +0300

    redux-form-init

---
+++ README.md
@@ -0,0 +1,3 @@
+Redux form with react
+
+just for try
---
+++ src/validation/index.js
@@ -0,0 +1,19 @@
+export const required = value => (value ? undefined : 'Value is required');
+
+export const minLength = value =>
+  value.length < 4 ? 'Value must be at least 4 characters' : undefined;
+
+export const maxLength = value => (value.length > 10 ? 'Value is too long' : undefined);
+
+export const matchesPassword = (value, allValues) =>
+  value === allValues.password ? undefined : 'Password mush match';
+
+export const asyncValidate = async values => {
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  await sleep(1000);
+  if (['kent', 'andy', 'john', 'joel'].includes(values.username)) {
+    return Promise.reject({
+      username: 'User already taken'
+    });
+  }
+};
